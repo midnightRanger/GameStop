7c5f0085d6b49480023181b296e03e84d7ba4c57
Lev Ostapenko <012938576a@gmail.com>
2022-12-10 19:39:50 +0300

    margin to input fields

--- GameStop/Controllers/ModeratorController.cs
+++ GameStop/Controllers/ModeratorController.cs
@@ -10,20 +10,28 @@ public class ModeratorController : Controller
     private readonly ILogger<HomeController> _logger;
     private readonly ApplicationContext _db;
     private readonly IReview _reviewRepository;
+    private IEnumerable<ReviewModel> _reviews;
 
     public ModeratorController(ILogger<HomeController> logger, ApplicationContext db, IReview reviewRepository)
     {
         _logger = logger;
         _db = db;
         _reviewRepository = reviewRepository;
+        _reviews = _reviewRepository.getAll().Include(r => r.Product).ThenInclude(p => p.ProductInfo)
+            .Include(r => r.Author).ThenInclude(a=>a.Account).ToList();
     }
     
     public async Task<IActionResult> Reviews(string? notification)
     {
         if(notification != null) 
             ModelState.AddModelError("", notification);
-        var reviews = await _reviewRepository.getAll().Include(r => r.Product).ThenInclude(p => p.ProductInfo)
-            .Include(r => r.Author).ToListAsync();
-        return View(reviews); 
+        return View(_reviews); 
+    }
+    
+    [HttpGet]
+    public IActionResult ReviewUpdate(int id)
+    {
+        var review = _reviews.FirstOrDefault(rev => rev.Id == id);
+        return View(review);
     }
 }
